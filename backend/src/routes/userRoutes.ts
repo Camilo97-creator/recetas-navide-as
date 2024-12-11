9434860969add801bb1156e90ab55f01523e488c
Camilo Munoz <mcamiloesteban67@gmail.com>
2024-12-11 17:22:37 -0500

    first commit

---
+++ backend/src/routes/userRoutes.ts
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+import { registrerUser, getAllUsers, getUserById } from '../controllers/userController';
+
+const router=Router();
+
+router.post('/register', registrerUser);
+router.get('/', getAllUsers);
+router.get('/:id', getUserById);
+
+export default router;
+
